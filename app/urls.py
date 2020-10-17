16dcb0517bd1ab6acf456f505e75b2a993eac0a1
Semen Syrovatskiy <semen.syrovatskiy@gmail.com>
2020-10-17 21:51:21 +0300

    Webhook settings

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("", HomePageView.as_view(), name='home'),
-    path("news/", NewsPageView.as_view(), name='news')
+    path("", HomePageView.as_view(), name="home"),
+    path("news/", NewsPageView.as_view(), name="news"),
+    path("webhook/", webhook)
 ]
--- app/views.py
+++ app/views.py
@@ -1,7 +1,20 @@
+import git
+from django.http import HttpResponse
 from django.views.generic import TemplateView
+from django.views.decorators.csrf import csrf_exempt
 
 class HomePageView(TemplateView):
     template_name = "home.html"
 
 class NewsPageView(TemplateView):
     template_name = "news.html"
+
+@csrf_exempt
+def webhook(request):
+    if request.method == "POST":
+        repo = git.Repo("/home/jonathanblade/django-project")
+        origin = repo.remotes.origin
+        origin.pull()
+        return HttpResponse("Updated PythonAnywhere successfully")
+    else:
+        return HttpResponse("Wrong event type")
